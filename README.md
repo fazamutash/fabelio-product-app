f1aeda7c0f004cea888b1fabfc498b4ad9ac98ac
fazamutash <fazamutash@gmail.com>
2021-08-28 18:33:50 +0700

    add readme and postbuild-script

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
+engine:
 node version 10.14.0
+npm version 6.14.8
+
+prerequisite:
+you have postgres installed on your local
+
+note:
+you need run it with two terminal.
+The first one is for backend running on port 5000.
+The second one is for frontend running on port 3000.
+
+steps:
+go to config folder
+create a file like config.example.json and name it to config.json
+update database configuration under development object on config.json
+
+Terminal 1:
+npm install
+sequelize db:migrate
+sequelize db:seed:all
+npm start
+
+Terminal 2:
+cd client
+npm install 
+npm start
+
+To run the test:
+npm run test
+
+
+deploy to heroku:
+make sure you have pgsql addons enabled
+to do this:
+heroku addons:create heroku-postgresql:hobby-dev -a application-name
+
+and then set PGSSLMODE to be require
+heroku config:set PGSSLMODE=require
+
+push to heroku
+git push heroku master
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-const bodyParser = require('body-parser');
-const Sequelize = require("sequelize");
-const dbConfig = require("./config/config.json").development;
 const Product = require("./models").Product;
 const connectToDatabase = require("./db");
 const apiRoutes = require('./routes/api');
@@ -42,32 +39,3 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => console.log("The node.js app is listening on port 5000."));
-
-
-/*async function connectToDatabase() {
-
-  try {
-    const connection = await mysql.createConnection({ 
-      host: dbConfig.host, 
-      port: dbConfig.port, 
-      user: dbConfig.username, 
-      password: dbConfig.password 
-    });
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${dbConfig.database}\`;`);
-    const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-      host: dbConfig.host,
-      dialect: dbConfig.dialect,
-      operatorsAliases: false,
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-    });
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.log("Unable to connect to the database:", error);
-  }
-}*/
