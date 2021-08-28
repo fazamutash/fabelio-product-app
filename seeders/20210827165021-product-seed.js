e9688de046412e915ecaa1a5af50a63bd83fb292
fazamutash <fazamutash@gmail.com>
2021-08-28 18:58:31 +0700

    prevent seed to run twice

--- seeders/20210827165021-product-seed.js
+++ seeders/20210827165021-product-seed.js
@@ -3,22 +3,32 @@
 const timestamp = new Date();
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert(
-      "Products",
-      [
-        {
-          link: 'https://fabelio.com/ip/meja-tamu-chloe-set-3-new',
-          name: 'Meja Tamu Chloe - Set of 3',
-          price: 'Rp1.044.450',
-          image: 'https://cdn-m2.fabelio.com/catalog/product/e/d/edt1_18.jpg?auto=format&w=1678',
-          details: 'this is from seed',
-          createdAt: timestamp,
-          updatedAt: timestamp,
-        },
-      ],
-      {}
-    );
+  up: async (queryInterface, Sequelize) => {
+    const seedProduct = await queryInterface.rawSelect('Products', {
+      where: {
+        id: 1,
+      },
+    }, ['id']);
+
+    if(!seedProduct) {
+      return queryInterface.bulkInsert(
+        "Products",
+        [
+          {
+            link: 'https://fabelio.com/ip/meja-tamu-chloe-set-3-new',
+            name: 'Meja Tamu Chloe - Set of 3',
+            price: 'Rp1.044.450',
+            image: 'https://cdn-m2.fabelio.com/catalog/product/e/d/edt1_18.jpg?auto=format&w=1678',
+            details: 'this is from seed',
+            createdAt: timestamp,
+            updatedAt: timestamp,
+          },
+        ],
+        {}
+      );
+    } else {
+      return true;
+    }
   },
 
   down: (queryInterface, Sequelize) => {
